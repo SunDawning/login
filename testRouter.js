a36bb8aa29fbba49f8d9835cc324f74d19e3ee45
SunDawning <dpmeichen@gmail.com>
2021-04-25 23:37:04 +0800

    修复 在没有登录之前，从文件里读取的TOKENS总是空的；记录每个测试的内容，并输出测试报告。

--- testRouter.js
+++ testRouter.js
@@ -1,6 +1,5 @@
 import{sendObjectInJSON}from"./sendObjectInJSON.js";
 import{consoleLog}from"./consoleLog.js";
-import{TOKENS}from"./TOKENS.js";
 import{ACCOUNTS}from"./ACCOUNTS.js";
 import{encrypt}from"./encrypt.js";
 /**
@@ -10,54 +9,86 @@ export async function testRouter(port){
     function getURL(path){
         return `http://localhost:${port}${path}`; // 网址的前缀
     }
-    let body=[];
+    let database=[];
     let validAccount=Object.keys(ACCOUNTS)[0];
     let random=new Date().getTime();
     /*
      * 测试＂/login＂，并记录token。
      */
-    body.push({
+    let loginResponse=await(await sendObjectInJSON(getURL("/login"),"POST",{
+        account:validAccount,
+        password:encrypt(ACCOUNTS[validAccount].password+random),
+        random:random,
+    })).json();
+    database.push({
         document:`POST /login`,
-        response:await(await sendObjectInJSON(getURL("/login"),"POST",{
-            account:validAccount,
-            password:encrypt(ACCOUNTS[validAccount].password+random),
-            random:random,
-        })).json(),
+        is:JSON.stringify(loginResponse),
     }); // 验证账号密码登录信息
     let inValidToken="inValidToken";
-    let validToken=Object.keys(TOKENS)[0];
-    // body.push({
-    //     document:`验证GET /login`,
-    //     response:await (await window.fetch(getURL("/login"))).json(),
-    // })
-    body.push({
+    let validToken=loginResponse.token;
+    database.push({
+        document:`验证GET /login`,
+        is:await (await window.fetch(getURL("/login"))).text(),
+        shouldbe:"",
+        form:`window.fetch("${getURL("/login")}")`,
+    })
+    database.push({
         document:`POST /login 使用有效的token`,
-        response:await (await sendObjectInJSON(getURL("/login"),"POST",{token:validToken})).json(),
+        is:await (await sendObjectInJSON(getURL("/login"),"POST",{token:validToken})).text(),
+        shouldbe:'{"isValidAccount":false,"isValidPassword":false,"isValidToken":true}',
+        form:`sendObjectInJSON("${getURL("/login")}","POST",{token:"${validToken}"})`,
     });
-    body.push({
+    database.push({
         document:`POST /login 使用无效的token`,
-        response:await (await sendObjectInJSON(getURL("/login"),"POST",{token:inValidToken})).json(),
+        is:await (await sendObjectInJSON(getURL("/login"),"POST",{token:inValidToken})).text(),
+        shouldbe:'{"isValidAccount":false,"isValidPassword":false,"isValidToken":false}',
+        form:`sendObjectInJSON("${getURL("/login")}","POST",{token:"${inValidToken}"})`,
     });
     /*
      * 测试＂/isValidToken＂
      */
-    body.push({
+    database.push({
         document:`POST /isValidToken 使用无效的token`,
-        response:await (await sendObjectInJSON(getURL("/isValidToken"),"POST",{token:inValidToken})).json(),
+        is:await (await sendObjectInJSON(getURL("/isValidToken"),"POST",{token:inValidToken})).text(),
+        shouldbe:'{"isValidToken":false}',
+        form:`sendObjectInJSON("${getURL("/isValidToken")}","POST",{token:"${inValidToken}"})`,
     });
-    body.push({
+    database.push({
         document:`POST /isValidToken 使用有效的token`,
-        response:await (await sendObjectInJSON(getURL("/isValidToken"),"POST",{token:validToken})).json(),
+        is:await (await sendObjectInJSON(getURL("/isValidToken"),"POST",{token:validToken})).text(),
+        shouldbe:'{"isValidToken":true}',
+        form:`sendObjectInJSON("${getURL("/isValidToken")}","POST",{token:"${validToken}"})`,
     });
-    body.push({
+    database.push({
         document:`GET /isValidToken 使用无效的token`,
-        response:await (await window.fetch(getURL(`/isValidToken?token=${inValidToken}`))).json(),
+        is:await (await window.fetch(getURL(`/isValidToken?token=${inValidToken}`))).text(),
+        shouldbe:'{"isValidToken":false}',
+        form:`window.fetch("${getURL("/isValidToken?token=${inValidToken}")}")`,
     });
-    body.push({
+    database.push({
         document:`GET /isValidToken 使用有效的token`,
-        response:await (await window.fetch(getURL(`/isValidToken?token=${validToken}`))).json(),
-    })
-    consoleLog("测试结果",body);
+        is:await (await window.fetch(getURL(`/isValidToken?token=${validToken}`))).text(),
+        shouldbe:'{"isValidToken":true}',
+        form:`window.fetch("${getURL("/isValidToken?token=${validToken}")}")`,
+    });
+    let report={
+        passed:[],
+        failed:[],
+    };
+    database.forEach(function(data){
+        let is=data.is;
+        let shouldbe=data.shouldbe;
+        if(is&&shouldbe&&(is===shouldbe)){
+            report.passed.push(data);
+        }else{
+            report.failed.push(data);
+        }
+    });
+    consoleLog("测试结果",Object.assign({
+        total:database.length,
+        passedTotal:report.passed.length,
+        failedTotal:report.failed.length,
+    },report));
 }
 import{PORT}from"./PORT.js";
 if(import.meta.main){
