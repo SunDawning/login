43009f97f459a8e2ad26096e73a0b10725bd0600
SunDawning <dpmeichen@gmail.com>
2021-04-25 22:38:13 +0800

    新增 显示测试的结果

--- testRouter.js
+++ testRouter.js
@@ -10,29 +10,53 @@ export async function testRouter(port){
     function getURL(path){
         return `http://localhost:${port}${path}`; // 网址的前缀
     }
-    let body={};
+    let body=[];
     let validAccount=Object.keys(ACCOUNTS)[0];
     let random=new Date().getTime();
     /*
      * 测试＂/login＂，并记录token。
      */
-    await sendObjectInJSON(getURL("/login"),"POST",{
-        account:validAccount,
-        password:encrypt(ACCOUNTS[validAccount].password+random),
-        random:random,
+    body.push({
+        document:`POST /login`,
+        response:await(await sendObjectInJSON(getURL("/login"),"POST",{
+            account:validAccount,
+            password:encrypt(ACCOUNTS[validAccount].password+random),
+            random:random,
+        })).json(),
     }); // 验证账号密码登录信息
     let inValidToken="inValidToken";
     let validToken=Object.keys(TOKENS)[0];
-    await window.fetch(getURL("/login")); // 验证GET /login
-    await sendObjectInJSON(getURL("/login"),"POST",{token:validToken});
-    await sendObjectInJSON(getURL("/login"),"POST",{token:inValidToken});
+    // body.push({
+    //     document:`验证GET /login`,
+    //     response:await (await window.fetch(getURL("/login"))).json(),
+    // })
+    body.push({
+        document:`POST /login 使用有效的token`,
+        response:await (await sendObjectInJSON(getURL("/login"),"POST",{token:validToken})).json(),
+    });
+    body.push({
+        document:`POST /login 使用无效的token`,
+        response:await (await sendObjectInJSON(getURL("/login"),"POST",{token:inValidToken})).json(),
+    });
     /*
      * 测试＂/isValidToken＂
      */
-    await sendObjectInJSON(getURL("/isValidToken"),"POST",{token:inValidToken});
-    await sendObjectInJSON(getURL("/isValidToken"),"POST",{token:validToken});
-    await window.fetch(getURL(`/isValidToken?token=${inValidToken}`));
-    await window.fetch(getURL(`/isValidToken?token=${validToken}`));
+    body.push({
+        document:`POST /isValidToken 使用无效的token`,
+        response:await (await sendObjectInJSON(getURL("/isValidToken"),"POST",{token:inValidToken})).json(),
+    });
+    body.push({
+        document:`POST /isValidToken 使用有效的token`,
+        response:await (await sendObjectInJSON(getURL("/isValidToken"),"POST",{token:validToken})).json(),
+    });
+    body.push({
+        document:`GET /isValidToken 使用无效的token`,
+        response:await (await window.fetch(getURL(`/isValidToken?token=${inValidToken}`))).json(),
+    });
+    body.push({
+        document:`GET /isValidToken 使用有效的token`,
+        response:await (await window.fetch(getURL(`/isValidToken?token=${validToken}`))).json(),
+    })
     consoleLog("测试结果",body);
 }
 import{PORT}from"./PORT.js";
