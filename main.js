0a75c53a809e6b356f19f9e8d3e56187a0db121c
SunDawning <dpmeichen@gmail.com>
2021-05-23 12:14:03 +0800

    新增 可以设置端口

--- main.js
+++ main.js
@@ -2,9 +2,33 @@
  * 登录界面
  * 在浏览器里使用程序
  */
+import{
+    parse
+}from"https://deno.land/std/flags/mod.ts";
+let commandLineArgs=parse(Deno.args);
+if(commandLineArgs.help){
+    console.log(`
+A Login Server to manage Login Page.
+
+Version: <2021-05-23 Sun 11:57:34 UTC+08:00>
+
+  USAGE:
+    HomeServer [options]
+
+  OPTIONS:
+    --help              Prints help information
+    --port <PORT>       Set port
+`);
+    Deno.exit();
+}
+import{formatTime}from"https://gitee.com/sundawning/deno-oak-rest-users/raw/1d10a561a22b57e7385f4c240bb88b6c3d3043f6/formatTime.js";
+console.log(formatTime(),"commandLineArgs",commandLineArgs);
 let hostname="0.0.0.0";
+let port=commandLineArgs.port;
 import{PORT}from"./PORT.js";
-let port=PORT;
+if(port===undefined){
+    port=PORT;
+}
 import{serve}from"https://deno.land/std@0.95.0/http/server.ts";
 let server=serve({
     hostname:hostname,
